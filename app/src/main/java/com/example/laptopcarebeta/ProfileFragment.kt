373a1c849e15d43c03e77be95d50c4906cbf39a9
trialakun <trial.akun1403@gmail.com>
2023-11-29 21:00:32 +0700

    test

--- app/src/main/java/com/example/laptopcarebeta/ProfileFragment.kt
+++ app/src/main/java/com/example/laptopcarebeta/ProfileFragment.kt
@@ -1,17 +1,28 @@
 package com.example.laptopcarebeta
 
 import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profilenamaakuntextview: TextView
     private lateinit var profileusernameakunvextview: TextView
+    private lateinit var profilebtnmyaccountlinearlayout: LinearLayout
+    private lateinit var profilebtnmylaptoplinearlayout: LinearLayout
+    private lateinit var profilebtnlogoutlinearlayout: LinearLayout
+    private lateinit var profilebtnaboutapplinearlayout: LinearLayout
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,6 +36,10 @@ class ProfileFragment : Fragment() {
 
         profilenamaakuntextview = view.findViewById(R.id.ProfileNamaAkunTextView)
         profileusernameakunvextview = view.findViewById(R.id.ProfileUsernameAkunTextView)
+        profilebtnmyaccountlinearlayout = view.findViewById(R.id.ProfileBtnMyAccountLinearLayout)
+        profilebtnmylaptoplinearlayout = view.findViewById(R.id.ProfileBtnMyLaptopLinearLayout)
+        profilebtnlogoutlinearlayout = view.findViewById(R.id.ProfileBtnLogOutLinearLayout)
+        profilebtnaboutapplinearlayout = view.findViewById(R.id.ProfileBtnAboutAppLinearLayout)
 
         val sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         val namaAkun = sharedPreferences.getString("nama_akun", "")
@@ -32,5 +47,25 @@ class ProfileFragment : Fragment() {
 
         profilenamaakuntextview.text = "$namaAkun"
         profileusernameakunvextview.text = "$userName"
+
+        profilebtnlogoutlinearlayout.setOnClickListener {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+            builder.setTitle("Konfirmasi Logout")
+            builder.setMessage("Anda yakin ingin logout?")
+
+            builder.setPositiveButton("Ya",
+                DialogInterface.OnClickListener { dialog, which -> // Lakukan logout atau action yang sesuai di sini
+                    Toast.makeText(context, "Kamu sudah logout", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                })
+
+            builder.setNegativeButton("Batal",
+                DialogInterface.OnClickListener { dialog, which -> // Batalkan atau tutup dialog jika tombol "Batal" ditekan
+                    dialog.dismiss()
+                })
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
+        }
     }
 }
